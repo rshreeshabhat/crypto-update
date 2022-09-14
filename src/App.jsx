035176546f297dbfd21a2bc2fbe27aa9671297b8
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-14 13:03:09 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1 className="text-center">App</h1>
+    </div>
+  )
+}
+
+export default App
