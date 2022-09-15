1adb71cc92686a66e4af4d73009855b22a895ba6
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-16 00:56:02 +0530

    9thh-commit

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -3,21 +3,21 @@ import { DownArrow, UpArrow } from '../icons/icons'
 import { Link } from 'react-router-dom'
 
 function currencyFormat(num) {
-    return '₹' + num.toFixed(4).toString()
+    return '₹' + Math.round(num)
 }
 
 function plusOrMinus(num) {
     if (num >= 0) {
         return (<>
             <div className="flex items-center gap-1 cursor-pointer">
-                <UpArrow /> <span className='font-semibold text-green-900'>{num.toFixed(4)}</span>
+                <UpArrow /> <span className='font-semibold text-green-900'>{num.toFixed(4)}%</span>
             </div>
         </>);
     }
     else {
         return (<>
             <div className="flex items-center gap-1 cursor-pointer">
-                <DownArrow /> <span className='font-semibold text-red-800'>{num.toFixed(4)}</span>
+                <DownArrow /> <span className='font-semibold text-red-800'>{num.toFixed(4)}%</span>
             </div>
         </>);
     }
@@ -49,7 +49,7 @@ const Coin = ({ coin }) => {
                     <p className='font-semibold'>{coin.name}</p><small className="font-xs"> ({coin.symbol})</small>
                 </div>
                 <span className="font-medium items-center">{currencyFormat(coin.current_price)}</span>
-                <span className="font-medium items-center">{plusOrMinus(coin.price_change_percentage_24h)}%</span>
+                <span className="font-medium items-center">{plusOrMinus(coin.price_change_percentage_24h)}</span>
                 <div className='hidden sm:block items-center'>
                     <p className='font-semibold'>Price Change In 24h</p>
                     <span>{plusOrMinusInr(coin.price_change_24h)}</span>
