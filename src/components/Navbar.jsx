873e107d8be8d97849b7a7a3821d1e8034344f1d
rshreeshabhat <rshreeshabhat@gmail.com>
2022-09-15 15:32:01 +0530

    5th-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { LogoIcon } from "../icons/icons"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="bg-black text-white h-14 flex items-center">
         <div className="wrapper-container  w-full">
+          <Link to={`/`}>
             <div className="flex items-center gap-1 cursor-pointer">
                 <LogoIcon/>
                 <p className="font-semibold"><span className="font-color-change">C</span>oin<span className="font-color-change">D</span>ata</p>
             </div>
+          </Link>
         </div>
     </div>
   )
